9c3a48a4696b9956bab34440db0045d2df65895a
hyooa <hyoyung3231@gmail.com>
2022-07-01 14:43:52 +0900

    server 구현

---
+++ models/Products.js
@@ -0,0 +1,25 @@
+// 내보내기 > Common.js 구문
+// module.exports
+module.exports = function (sequelize, DataTypes) {
+    // 컬럼 > name, price, imageUrl, seller
+    // 제약 조건 > allowNull : 컬럼의 값이 없어도 되는지 여부 (default : true / false - 허용X)
+    // https://github.com/design-view/lamp-shopping-server
+    const product = sequelize.define('Product', { // define > 테이블 만들어줌
+        name : {
+            type : DataTypes.STRING(20),
+            allowNull : false,
+        },
+        price : {
+            type : DataTypes.INTEGER(10),
+            allowNull : false,
+        },
+        imageUrl : {
+            type : DataTypes.STRING(500),
+        },
+        seller : {
+            type : DataTypes.STRING(200),
+            allowNull : false,
+        },
+    });
+    return product;
+}
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express(); // 실행, app에 결과 담김
 const port = 3000;
+const models = require('./models'); // 불러와서 사용 💜
 
 // json 형식의 데이터를 처리할 수 있게 설정
 app.use(express.json());
@@ -12,71 +13,55 @@ app.use(cors()); // 브라우저 cors 이슈를 막기위해 사용(모든 브�
 
 // 요청처리
 // app.메서드(url, 함수)
-// 🧡 get 요청
+// 🧡 get 요청products
 app.get('/products', async(req, res) => { // '/products' 경로
-    const result = {
-        products : [
-            {
-                id : 1,
-                name : "원목 의자",
-                price : 70000,
-                imgsrc : "images/products/product1.jpg",
-                seller : "초록이"
-            },
-            {
-                id : 2,
-                name : "원목 의자",
-                price : 70000,
-                imgsrc : "images/products/product2.jpg",
-                seller : "초록이"
-            },
-            {
-                id : 3,
-                name : "원목 의자",
-                price : 70000,
-                imgsrc : "images/products/product3.jpg",
-                seller : "초록이"
-            },
-            {
-                id : 4,
-                name : "원목 의자",
-                price : 70000,
-                imgsrc : "images/products/product4.jpg",
-                seller : "초록이"
-            },
-            {
-                id : 5,
-                name : "원목 의자",
-                price : 70000,
-                imgsrc : "images/products/product1.jpg",
-                seller : "초록이"
-            },
-            {
-                id : 6,
-                name : "원목 의자",
-                price : 70000,
-                imgsrc : "images/products/product2.jpg",
-                seller : "초록이"
-            },
-            {
-                id : 7,
-                name : "원목 의자",
-                price : 70000,
-                imgsrc : "images/products/product3.jpg",
-                seller : "초록이"
-            },
-            {
-                id : 8,
-                name : "원목 의자",
-                price : 70000,
-                imgsrc : "images/products/product4.jpg",
-                seller : "초록이"
-            },
-        ]
-    }
+    
+    // 💜 데이터 베이스 다 조회하기 > findAll()
+    models.Product.findAll()
+    .then( // 조회 결과를 받음
+        result => {
+            console.log("제품 전체 조회 ", result);
+            res.send(result);
+        }
+    )
+    .catch(e => {
+        console.error(e);
+        res.send('파일 조회에 문제있음');
+    })
+
     // res.send('업로드된 상품들 입니다.'); // 응답해줌
-    res.send(result);
+    // res.send(result);
 })
+
+// 💜 위(/products)와 아래(/product/:id), 요청 경로가 다름
+
+// method get요청이 오고 url은 /product/2(id) 이렇게 요청이 왔을 때
+                        // 띄우기
+app.get('/product/:id', async (req, res) => {
+    const params = req.params; // 객체
+    // const { id } = params; // 객체 구조분해 할당
+
+    // 하나만 조회할 때 > findOne --> select문
+    models.Product.findOne({
+        // 조건절
+        where : {
+            id : params.id,
+        }
+    })
+    .then(result => {
+        res.send(result); // 객체로 받아옴
+    })
+    .catch( e => { // 결과 없을 때 화면에 안보여줌 ..'3'
+        console.log(e);
+        res.send('상품 조회에 문제가 생김')
+    })
+    
+    // 전송
+    // res.send(product);
+
+    // 주소창에 확인
+    // http://localhost:3000/product/:2
+});
 
 // 🧡 메서드 post로 요청 왔을 때
 app.post('/green', async(req, res) => {
@@ -86,6 +71,20 @@ app.post('/green', async(req, res) => {
 
 // 실행
 app.listen(port, () => {
-
     console.log('쇼핑몰 서버가 동작중입니다 ~ing');
+
+    // 💜 Sequelize 와 데이터베이스 연결 작업
+    // 데이터베이스 동기화
+    // php 처럼 (?)
+    models.sequelize
+    .sync()
+    .then(() => {
+        console.log('DB연결 성공');
+    })
+    .catch(e => {
+        console.log(e);
+        console.log('DB연결 에러');
+        // 서버실행이 안되면 프로세서를 종료
+        process.exit();
+    })
 })
